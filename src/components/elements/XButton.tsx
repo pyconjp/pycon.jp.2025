62f42f04a915c9d33e934c1101f5e893a7193736
Takuma Tateishi <takutate.0427@gmail.com>
2025-05-26 23:44:39 +0900

    スペースが狭い問題を修正

--- src/components/elements/XButton.tsx
+++ src/components/elements/XButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function XButton() {
   return (
     <Link href={"https://x.com/pyconjapan"} target="_blank" rel="noopener noreferrer nofollow">
-      <button className="border-[1px] border-black/30 font-bold min-w-[140px] min-h-[50px] py-2 px-4 rounded-full flex items-center justify-center space-x-2">
+      <button className="border-[1px] border-black/30 font-bold min-w-[140px] min-h-[50px] py-2 px-4 rounded-full flex items-center justify-center space-x-4">
         <span className="text-black">X</span>
         <span className="text-black/60">@pyconjapan</span>
       </button>
