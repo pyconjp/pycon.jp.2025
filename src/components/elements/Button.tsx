052263f23f3f1c4330fab5c635589902c4c08fa9
Takuma Tateishi <takutate.0427@gmail.com>
2025-05-27 18:43:44 +0900

    [Fix] 微調整を行いました

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Button({ label, link }: Props) {
   return (
     <Link as="a" href={link} target="_blank" rel="noopener noreferrer nofollow">
-      <button className="border-[1px] border-black/20 font-bold min-w-[140px] min-h-[50px] py-2 px-4 rounded-full">
+      <button className="border-[1px] border-black/20 font-bold min-w-[140px] min-h-[50px] py-2 px-4 rounded-full flex items-center justify-center">
         {label}
       </button>
     </Link>
--- src/components/elements/FixedMenu.tsx
+++ src/components/elements/FixedMenu.tsx
@@ -6,7 +6,7 @@ export default function FixedMenu() {
     <>
       <div className="z-10 fixed bottom-0 w-full h-[310px] p-4">
         <div className="absolute inset-0 bg-[linear-gradient(160deg,transparent_61.52%,black_190.45%)] blur-md -z-10"></div>
-        <div className="absolute right-4 bottom-8 flex space-x-6 p-4 px-6 backdrop-blur-md bg-white rounded-full">
+        <div className="absolute right-4 bottom-6 flex space-x-4 p-2 px-4 backdrop-blur-md bg-white rounded-full">
           <XButton/>
           <GradationButton label={"チケット購入"} link={"#"} />
         </div>
