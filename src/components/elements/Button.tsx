5cc570c032eff3b72d0e2ef12e54cc820e7e6748
k-nakamura <koya.nakamura@gmail.com>
2025-06-08 11:38:56 +0900

    add ExternalLink component

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import ExternalLink from "@/components/elements/ExternalLink";
 
 type Props = {
   label: string;
@@ -7,10 +7,10 @@ type Props = {
 
 export default function Button({ label, link }: Props) {
   return (
-    <Link as="a" href={link} target="_blank" rel="noopener noreferrer nofollow">
+    <ExternalLink href={link} as='a'>
       <button className="border-[1px] border-black/20 font-bold min-w-[140px] min-h-[50px] py-2 px-4 rounded-full flex items-center justify-center cursor-pointer">
         {label}
       </button>
-    </Link>
+    </ExternalLink>
   );
 }
