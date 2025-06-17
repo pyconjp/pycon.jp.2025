99dc7000fc9919d089cf3b785dcecbf2f9dceee0
Takuma Tateishi <takutate.0427@gmail.com>
2025-06-17 23:05:51 +0900

    [Fix] 横スクロールできない問題を修正

--- src/components/sections/Navi_about.tsx
+++ src/components/sections/Navi_about.tsx
@@ -6,17 +6,16 @@ interface Navi_aboutProps {
 
 export default function Navi_about({ position }: Navi_aboutProps) {
   return (
-    <nav className="flex justify-center space-x-8 text-sm font-semibold my-6 overflow-x-scroll">
+    <nav className="flex sm:justify-center space-x-8 text-sm font-semibold my-6 overflow-x-scroll flex-nowrap px-4">
       {["会場案内", "行動規範", "メンバー", "スポンサー", "お知らせ"].map((item, index) => (
         <Link
           key={index}
           href={`/${item}`}
-          className={`relative pb-2 after:absolute after:left-1/2 after:-translate-x-1/2 after:bottom-0 after:w-6 after:h-[2px] after:bg-black after:transition-opacity after:duration-300 ${position === item ? "after:opacity-100" : "after:opacity-0 hover:after:opacity-100"}`}
+          className={`relative pb-2 after:absolute after:left-1/2 after:-translate-x-1/2 after:bottom-0 after:w-6 after:h-[2px] after:bg-black after:transition-opacity after:duration-300 ${position === item ? "after:opacity-100" : "after:opacity-0 hover:after:opacity-100"} whitespace-nowrap`}
         >
           {item}
         </Link>
       ))}
     </nav>
-
   )
 }
