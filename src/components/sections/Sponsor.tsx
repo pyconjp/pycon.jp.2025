a5df7ece0e573d7f403099b90f47a49e4695012a
Takuma Tateishi <takutate.0427@gmail.com>
2025-06-21 01:50:10 +0900

    [Add] Sponsorセクションの作成

---
+++ src/components/sections/Sponsor.tsx
@@ -0,0 +1,112 @@
+import { Lang } from "@/types/lang";
+import { Sponsor } from "@/types/sponsor";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  sponsors: Sponsor[];
+  lang: Lang;
+} & React.HTMLAttributes<HTMLElement>;
+
+const platinum_len = 60;
+const gold_len = 20;
+
+export default function SponsorSection({ sponsors, lang, ...props }: Props) {
+  return (
+    <section {...props}>
+      <div className="relative">
+        <h2 className="flex max-lg:flex-col max-lg:gap-6 lg:items-center my-20">
+          <span className="text-5xl font-bold">Platinum Sponsors</span><span className="lg:mx-24">プラチナムスポンサー</span>
+        </h2>
+        <div className="grid lg:grid-cols-2 gap-24 space-y-12 justify-center items-center">
+          {sponsors.filter(sponsor => sponsor.plan === 'platinum').map((sponsor, index) => (
+            <div key={index} className="max-w-[315px] max-h-[215px] lg:max-w-[480px] lg:max-h-[210px]">
+              <div className="flex flex-col items-center bg-white border border-[#0000001A] rounded-lg">
+                <Image src={`/common/sponsor/${sponsor.logo_image}` || `/common/logo_pc.png`}
+                  alt={lang === 'ja' ? sponsor.name_ja : sponsor.name_en}
+                  width={480}
+                  height={210}
+                  className="w-72 h-42 object-contain" />
+              </div>
+              <div className="flex flex-col my-4">
+                <h3 className="text-xl font-bold">{lang === 'ja' ? sponsor.name_ja : sponsor.name_en}</h3>
+                <p className="lg:max-w-[480px] mt-4">
+                  {lang === 'ja' ? sponsor.pr_ja ? sponsor.pr_ja.substring(0, platinum_len) + "..." : "" : sponsor.pr_en ? sponsor.pr_en.substring(0, platinum_len) + "..." : ""}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative py-2">
+        <h2 className="flex max-lg:flex-col max-lg:gap-6 lg:items-center my-20">
+          <span className="text-5xl font-bold">PSF</span><span className="lg:mx-24">PSF</span>
+        </h2>
+        <div className="grid lg:grid-cols-2 gap-24 space-y-12 justify-center items-center">
+          {sponsors.filter(sponsor => sponsor.plan === 'psf').map((sponsor, index) => (
+            <div key={index} className="max-w-[315px] max-h-[215px] lg:max-w-[480px] lg:max-h-[210px]">
+              <div className="flex flex-col items-center bg-white border border-[#0000001A] rounded-lg">
+                <Image src={`/common/sponsor/${sponsor.logo_image}` || `/common/logo_pc.png`}
+                  alt={lang === 'ja' ? sponsor.name_ja : sponsor.name_en}
+                  width={480}
+                  height={210}
+                  className="w-72 h-42 object-contain" />
+              </div>
+              <div className="flex flex-col my-4">
+                <h3 className="text-xl font-bold">{lang === 'ja' ? sponsor.name_ja : sponsor.name_en}</h3>
+                <p className="lg:max-w-[480px]">
+                  {lang === 'ja' ? sponsor.pr_ja ? sponsor.pr_ja.substring(0, platinum_len) + "..." : "" : sponsor.pr_en ? sponsor.pr_en.substring(0, platinum_len) + "..." : ""}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative py-2">
+        <h2 className="flex max-lg:flex-col max-lg:gap-6 lg:items-center my-20">
+          <span className="text-5xl font-bold">Gold Sponsors</span><span className="lg:mx-24">ゴールドスポンサー</span>
+        </h2>
+        <div className="grid lg:grid-cols-4 gap-10 space-y-24 justify-center items-center">
+          {sponsors.filter(sponsor => sponsor.plan === 'gold').map((sponsor, index) => (
+            <div key={index} className="max-w-[225px] max-h-[220px] lg:max-w-[230px] lg:max-h-[220px]">
+              <div className="flex flex-col items-center bg-white border border-[#0000001A] rounded-lg">
+                <Image src={`/common/sponsor/${sponsor.logo_image}` || `/common/logo_pc.png`}
+                  alt={lang === 'ja' ? sponsor.name_ja : sponsor.name_en}
+                  width={480}
+                  height={210}
+                  className="w-52 h-42 object-contain" />
+              </div>
+              <div className="flex flex-col my-4">
+                <h3 className="text-xl font-bold">{lang === 'ja' ? sponsor.name_ja : sponsor.name_en}</h3>
+                <p className="lg:max-w-[230px] pt-2">
+                  {lang === 'ja' ? sponsor.pr_ja ? sponsor.pr_ja.substring(0, gold_len) + "..." : "" : sponsor.pr_en ? sponsor.pr_en.substring(0, gold_len) + "..." : ""}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative py-2">
+        <h2 className="flex max-lg:flex-col max-lg:gap-6 lg:items-center my-20">
+          <span className="text-5xl font-bold">Silver Sponsors</span><span className="lg:mx-24">シルバースポンサー</span>
+        </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 justify-center items-center">
+          {sponsors.filter(sponsor => sponsor.plan === 'silver').map((sponsor, index) => (
+            <div key={index} className="max-w-[145px] h-[220px] lg:max-w-[230px] lg:h-[160px]">
+              <div className="flex flex-col items-center bg-white border border-[#0000001A] rounded-lg">
+                <Image src={`/common/sponsor/${sponsor.logo_image}` || `/common/logo_pc.png`}
+                  alt={lang === 'ja' ? sponsor.name_ja : sponsor.name_en}
+                  width={480}
+                  height={210}
+                  className="w-32 h-22 object-contain p-2" />
+              </div>
+              <div className="flex flex-col my-4">
+                <h3 className="text-xl font-bold">{lang === 'ja' ? sponsor.name_ja : sponsor.name_en}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/libs/spreadsheet.ts
@@ -0,0 +1,53 @@
+import { Sponsor } from "@/types/sponsor";
+import { google } from "googleapis";
+
+const auth = new google.auth.JWT(
+  {
+    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    scopes: ['https://www.googleapis.com/auth/drive']
+  });
+
+const fetchSheet: <T extends {}>(spreadSheetId: string, range: string, keys: (keyof T)[]) => Promise<T[]>
+  = async <T extends {}>(spreadSheetId: string, range: string, keys: (keyof T)[]): Promise<T[]> => {
+    const sheets = google.sheets({ version: 'v4', auth });
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: spreadSheetId,
+      range,
+    });
+
+    return (response.data.values || [])
+      .map((row: string[]) => {
+        // rowの長さがkeysの長さに満たない場合、空文字列で埋める
+        while (row.length < keys.length) {
+          row.push('');
+        }
+
+        // 初期値を空のオブジェクトとして型推論を利用
+        return keys.reduce((acc, key, i) => {
+          acc[key] = row[i] as T[keyof T];
+          return acc;
+        }, {} as T);
+      });
+  }
+
+export async function getSponsors(): Promise<Sponsor[]> {
+  if (!process.env.SPONSOR_SPREADSHEET_ID) {
+    return [];
+  }
+  const sponsors: Sponsor[] = await fetchSheet<Sponsor>(
+    process.env.SPONSOR_SPREADSHEET_ID || '',
+    'Webサイト掲載用!A2:H100',
+    [
+      'name_ja',
+      'name_en',
+      'url_ja',
+      'url_en',
+      'pr_ja',
+      'pr_en',
+      'logo_image',
+      'plan',
+    ]
+  );
+  return sponsors;
+}
