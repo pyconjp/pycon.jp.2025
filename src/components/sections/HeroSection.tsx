e32bba0ef5d0f3adde9c1d30f7059bf380dc7fb1
k-nakamura <koya.nakamura@gmail.com>
2025-06-21 17:14:44 +0900

    resize hero section image

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -61,7 +61,7 @@ export default function HeroSection({lang}: {lang: Lang}) {
         >
           {images.map((src, index) => (
             <SwiperSlide key={index}>
-              <Image src={src} alt={`Hero Section ${index}`} width={4096} height={2731}
+              <Image src={src} alt={`Hero Section ${index}`} width={1024} height={683}
                      className='w-full h-full object-cover' loading='lazy'/>
             </SwiperSlide>
           ))}
