cd63e9c650f672a3186d5f46a93d818c9a18a32a
k-nakamura <koya.nakamura@gmail.com>
2025-06-22 01:21:35 +0900

    fix news link

--- src/components/sections/NewsSection.tsx
+++ src/components/sections/NewsSection.tsx
@@ -15,24 +15,25 @@ export default function NewsSection({lang, posts, ...props}: Props) {
       </h1>
       <div className='grid lg:grid-cols-2 grid-cols-1 gap-5 mt-11'>
         {posts.map((post) => (
-          <div key={post.url} className="flex flex-col justify-between p-5 border-gray-100 border-2 rounded-2xl min-h-36">
-            <h2 className="text-lg font-semibold">
-              <ExternalLink href={post.url}>
+          <ExternalLink key={post.url} href={post.url} className="block">
+            <article key={post.url}
+                  className="flex flex-col justify-between p-5 border-gray-100 border-2 rounded-2xl min-h-36">
+              <h2 className="text-lg font-semibold">
                 {post.title}
-              </ExternalLink>
-            </h2>
-            <time className="text-sm text-gray-500" dateTime={post.published}>
-              {new Date(post.published).toLocaleDateString(lang === 'ja' ? "ja-JP" : "en-US")}
-            </time>
-          </div>
+              </h2>
+              <time className="text-sm text-gray-500" dateTime={post.published}>
+                {new Date(post.published).toLocaleDateString(lang === 'ja' ? "ja-JP" : "en-US")}
+              </time>
+            </article>
+          </ExternalLink>
         ))}
-        <div className='bg-gray-50 border-gray-300 border-2 flex items-center justify-center rounded-2xl min-h-36'>
-          <ExternalLink href='https://pyconjp.blogspot.com/search/label/pyconjp2025'>
+        <ExternalLink href='https://pyconjp.blogspot.com/search/label/pyconjp2025'>
+          <span className='bg-gray-50 border-gray-300 border-2 flex items-center justify-center rounded-2xl min-h-36'>
             <span className="text-lg font-semibold">
               すべて
             </span>
-          </ExternalLink>
-        </div>
+          </span>
+        </ExternalLink>
       </div>
     </section>
   );
