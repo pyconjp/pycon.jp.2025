84f06c9a144d7c1a4d2c2551b21efc86aad5b599
k-nakamura <koya.nakamura@gmail.com>
2025-06-21 10:58:30 +0900

    set error image

--- src/components/elements/ImageWithFallback.tsx
+++ src/components/elements/ImageWithFallback.tsx
@@ -1,16 +1,32 @@
 import Image from "next/image";
 import {ComponentProps, useState} from "react";
 
-export default function ImageWithFallback({fallback = '/common/no_image.jpg', ...props}: ComponentProps<typeof Image> & {fallback ?: string}) {
+export default function ImageWithFallback({
+                                            fallback = '/common/no_image.jpg',
+                                            ...props
+                                          }: ComponentProps<typeof Image> & { fallback?: string }) {
   const [isError, setIsError] = useState(false)
 
   return (
-    <Image
-      {...props}
-      alt={props.alt}
-      onError={() => setIsError(true)}
-      src={isError ? fallback : props.src}
-      unoptimized
-    />
+    <>
+      {
+        isError ? (
+          <Image
+            {...props}
+            alt={props.alt}
+            src={fallback}
+            unoptimized
+          />
+        ) : (
+          <Image
+            {...props}
+            alt={props.alt}
+            onError={() => setIsError(true)}
+            src={props.src}
+            unoptimized
+          />
+        )
+      }
+    </>
   )
 }
