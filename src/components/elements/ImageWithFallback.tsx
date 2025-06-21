3fb45d57793e150a292c09e158879ef5c016656d
k-nakamura <koya.nakamura@gmail.com>
2025-06-21 09:55:47 +0900

    set error image

--- src/components/elements/ImageWithFallback.tsx
+++ src/components/elements/ImageWithFallback.tsx
@@ -2,17 +2,14 @@ import Image from "next/image";
 import {ComponentProps, useState} from "react";
 
 export default function ImageWithFallback({fallback = '/common/no_image.jpg', ...props}: ComponentProps<typeof Image> & {fallback ?: string}) {
-  const [src, setSrc] = useState(props.src);
+  const [isError, setIsError] = useState(false)
 
   return (
     <Image
       {...props}
       alt={props.alt}
-      onError={() => {
-        console.error('Image failed to load:', props.src);
-        setSrc(fallback);
-      }}
-      src={src}
+      onError={() => setIsError(true)}
+      src={isError ? fallback : props.src}
       unoptimized
     />
   )
