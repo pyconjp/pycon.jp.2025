2489644d4acd648c9734a871dd7242941dd98506
k-nakamura <koya.nakamura@gmail.com>
2025-06-21 18:03:39 +0900

    Add RecruitmentSection

--- src/components/sections/RecruitmentSection.tsx
+++ src/components/sections/RecruitmentSection.tsx
@@ -11,8 +11,8 @@ export default function RecruitmentSection({lang, ...props}: Props) {
 
   return (
     <section {...props}
-             className={clsx('flex lg:flex-row flex-col p-16 gap-10 bg-gray-100 border-2 border-gray-300 rounded-xl', props.className)}>
-      <div className='flex-1 space-y-12'>
+             className={clsx('flex lg:flex-row flex-col p-6 lg:p-16 gap-10 bg-gray-100 border-2 border-gray-300 rounded-xl', props.className)}>
+      <div className='flex-1 lg:space-y-12 space-y-6'>
         <h2 className='text-4xl font-semibold'>スピーカー募集</h2>
         <RecruitmentProposal/>
         <LinkButton href='https://pretalx.com/pycon-jp-2025/cfp'>
