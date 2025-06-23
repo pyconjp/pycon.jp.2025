e3ca453c9d12bebe5fcf5a117767ad17fdb297dc
k-nakamura <koya.nakamura@gmail.com>
2025-06-23 20:58:50 +0900

    fix header

--- src/components/elements/PageHead.tsx
+++ src/components/elements/PageHead.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 
 export default function PageHead({title, description, pagePath, imagePath = 'common/ogp/default.png', lang}: Props) {
-  const baseUrl = process.env.NEXT_PUBLIC_PRODUCTION_URL || process.env.CF_PAGES_URL || 'http://localhost:3000';
+  const baseUrl = process.env.NEXT_PUBLIC_PRODUCTION_URL || process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
   return (
     <Head>
