14b42242abbf4f15ab8ad9fd7bbc981807515029
Takuma Tateishi <takutate.0427@gmail.com>
2025-06-25 19:19:11 +0900

    [Fix] スマホで見た際にメニューバーの挙動がおかしくなる問題を修正

--- src/components/elements/FixedMenu.tsx
+++ src/components/elements/FixedMenu.tsx
@@ -9,9 +9,9 @@ export default function FixedMenu({lang}: {lang: Lang}) {
   return (
     <>
       <div className="z-10 fixed bottom-0 w-full h-[310px] p-4 pointer-events-none">
-        <div className="absolute inset-0 bg-[linear-gradient(160deg,transparent_61.52%,black_190.45%)] blur-md -z-10"></div>
+        <div className="absolute inset-0 bg-[linear-gradient(180deg,transparent_61.52%,black_190.45%)] lg:bg-[linear-gradient(160deg,transparent_61.52%,black_190.45%)] blur-md -z-10"></div>
       </div>
-      <div className="z-20 fixed right-4 bottom-6 flex space-x-4 p-2 px-4 backdrop-blur-md bg-white rounded-full">
+      <div className="z-20 fixed max-lg:right-0 max-lg:left-0 max-lg:mx-2 lg:right-4 bottom-2 lg:bottom-6 flex space-x-6 lg:space-x-4 p-2 lg:px-4 backdrop-blur-md bg-white rounded-full items-center justify-center">
         <XButton/>
         <GradationButton label={dict.button.buy_ticket} link={"#"} />
       </div>
