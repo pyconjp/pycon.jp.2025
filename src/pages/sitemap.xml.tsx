5daaa62280e605f6914a870a4d91895296b1681d
Takuma Tateishi <takutate.0427@gmail.com>
2025-07-01 20:46:07 +0900

    [Add] Sitemapを追加

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,75 @@
+import { getMembers, getSponsors } from '@/libs/spreadsheet';
+import { GetServerSidePropsContext } from 'next';
+
+async function generateSitemapXml(): Promise<string> {
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>`;
+  const buildTimestamp = new Date().toISOString()
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+  xml += `
+      <url>
+        <loc>https://2025.pycon.jp/</loc>
+        <lastmod>${buildTimestamp}</lastmod>
+        <changefreq>daily</changefreq>
+      </url>
+    `;
+  xml += `
+      <url>
+        <loc>https://2025.pycon.jp/members</loc>
+        <lastmod>${buildTimestamp}</lastmod>
+        <changefreq>weekly</changefreq>
+      </url>
+    `;
+  xml += `
+      <url>
+        <loc>https://2025.pycon.jp/sponsors</loc>
+        <lastmod>${buildTimestamp}</lastmod>
+        <changefreq>weekly</changefreq>
+      </url>
+    `;
+  xml += `
+      <url>
+        <loc>https://2025.pycon.jp/coc</loc>
+        <lastmod>${buildTimestamp}</lastmod>
+        <changefreq>yearly</changefreq>
+      </url>
+    `;
+  // ここでurlを足していく
+  const sponsors = await getSponsors();
+  const members = await getMembers();
+  sponsors.forEach((sponsor) => {
+    xml += `
+      <url>
+        <loc>https://2025.pycon.jp/sponsors/${sponsor.path}</loc>
+        <lastmod>${buildTimestamp}</lastmod>
+        <changefreq>weekly</changefreq>
+      </url>
+    `;
+  });
+  members.forEach((member) => {
+    xml += `
+      <url>
+        <loc>https://2025.pycon.jp/members/${member.path}</loc>
+        <lastmod>${buildTimestamp}</lastmod>
+        <changefreq>weekly</changefreq>
+      </url>
+    `;
+  });
+  xml += `</urlset>`;
+  return xml;
+}
+
+export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
+  const xml = await generateSitemapXml(); // xmlコードを生成する処理（後で書く）
+
+  res.statusCode = 200;
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // 24時間のキャッシュ
+  res.setHeader('Content-Type', 'text/xml');
+  res.end(xml);
+
+  return {
+    props: {},
+  };
+};
+
+const Page = () => null;
+export default Page;
