6d39e9af8fa5e2574aba7a1b52472c0f753ab8cb
Takuma Tateishi <takutate.0427@gmail.com>
2025-07-01 22:56:18 +0900

    [Add] Twitterカードのサポートを追加

--- src/components/elements/PageHead.tsx
+++ src/components/elements/PageHead.tsx
@@ -8,20 +8,25 @@ type Props = {
   lang: 'ja' | 'en';
 }
 
-export default function PageHead({title, description, pagePath, imagePath = '/common/ogp/default.png', lang}: Props) {
+export default function PageHead({ title, description, pagePath, imagePath = '/common/ogp/default.png', lang }: Props) {
   const baseUrl = process.env.NEXT_PUBLIC_PRODUCTION_URL || process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
   return (
     <Head>
       <title>{title ? (title + ' | PyCon JP 2025') : 'PyCon JP 2025'}</title>
-      <meta httpEquiv="Content-Language" content={lang}/>
-      <meta property="og:title" content={title}/>
-      <meta property="og:description" content={description || ''}/>
-      <meta property="og:image" content={`${baseUrl}${imagePath}`}/>
-      <meta property="og:url" content={`${baseUrl}${pagePath}`}/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:locale" content={lang === 'en' ? 'en_US' : 'ja_JP'}/>
-      <meta name="twitter:card" content="summary_large_image"/>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description || ''} />
+      <meta property="og:image" content={`${baseUrl}${imagePath}`} />
+      <meta property="og:url" content={`${baseUrl}${pagePath}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="PyCon JP 2025" />
+      <meta property="og:locale" content={lang === 'en' ? 'en_US' : 'ja_JP'} />
+      {/* --- Twitter Card Tags --- */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@pyconjapan" />
+      <meta name="twitter:title" content={title ? (title + ' | PyCon JP 2025') : 'PyCon JP 2025'} />
+      <meta name="twitter:description" content={description || ''} />
+      <meta name="twitter:image" content={`${baseUrl}${imagePath}`} />
     </Head>
   );
 }
