3eb0c184dec28daca2b2d316974121ece0e93da1
Takuma Tateishi <takutate.0427@gmail.com>
2025-07-13 02:25:41 +0900

    [Add] Venueページの作成

---
+++ src/components/sections/VenueSection.tsx
@@ -0,0 +1,59 @@
+import { Lang } from "@/types/lang";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import clsx from "clsx";
+import { dictionary } from "@/lang";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+type Props = { lang: Lang } & React.HTMLAttributes<HTMLElement>;
+
+export default function VenueSection({ lang, ...props }: Props) {
+  const dict = dictionary[lang];
+  const Description = dynamic(() => import(`@/components/markdown/${lang}/venue.mdx`), { ssr: true });
+
+  return (
+    <>
+      <section {...props} className={clsx('flex lg:flex-row flex-col gap-14 items-center justify-center', props.className)}>
+        <div className="relative flex gap-4 lg:flex-row flex-col lg:max-h-[730px] lg:gap-[58px]">
+          <div
+            className={clsx(
+              "relative flex flex-col gap-4 lg:w-[378px]",
+              lang === "ja" ? "lg:gap-18" : "lg:gap-10"
+            )}
+          >
+            <h3 className={clsx(
+              "font-extrabold max-lg:text-4xl",
+              lang === "ja" ? "text-xl" : "text-2xl"
+            )}>
+              {dict.venue.this_year_place}
+            </h3>
+            <p className="text-justify h-full">
+              <Description />
+            </p>
+            <div className={clsx("flex flex-col gap-4", lang === "ja" && "mt-4")}>
+              <Link href="#access" className="flex justify-between items-center border-b border-gray-300 py-3 pr-2 no-underline">
+                <span className="font-bold">{dict.venue.access_map_title}</span>
+                <span><FontAwesomeIcon icon={faArrowDown} /></span>
+              </Link>
+              <Link href="#hall" className="flex justify-between items-center border-b border-gray-300 py-3 pr-2 no-underline">
+                <span className="font-bold">{dict.venue.hall_map_title}</span>
+                <span><FontAwesomeIcon icon={faArrowDown} /></span>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <Image
+              src="/common/venue_hero.png"
+              alt="Venue Image Main"
+              width={740}
+              height={564}
+              className="max-lg:mt-12 w-full h-[265px] lg:w-[564px] lg:h-full max-lg:object-cover rounded-2xl"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
---
+++ src/pages/[lang]/venue/index.tsx
@@ -0,0 +1,48 @@
+import Navi_about from "@/components/elements/Navi_about";
+import PageHead from "@/components/elements/PageHead";
+import DefaultLayout from "@/components/layout/DefaultLayout";
+import { Lang } from "@/types/lang";
+import { GetStaticProps } from "next";
+import ContentsHeader from "@/components/sections/ContentsHeader";
+import VenueSection from "@/components/sections/VenueSection";
+import VenueDetailSection from "@/components/sections/VenueDetailSection";
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { lang: 'ja' } },
+      { params: { lang: 'en' } },
+    ],
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const lang = params?.lang || 'ja';
+  return {
+    props: {
+      lang,
+    },
+    revalidate: 3600,
+  };
+};
+
+function VenuePage({ lang }: { lang: Lang}) {
+  return (
+    <DefaultLayout lang={lang} activeHeader="about">
+      <Navi_about position="venue" lang={lang} />
+      <PageHead
+        title={lang === "ja" ? '会場案内' : 'Venue Information'}
+        description={lang === "ja" ? 'PyCon JP 2025の会場案内ページです' : 'This is the Venue Information page of PyCon JP 2025'}
+        lang={lang}
+        pagePath='/venue'
+      />
+      <ContentsHeader title="Venue" subtitle="会場案内"
+        images={['/common/venue_header_1.png', '/common/venue_header_2.png']} />
+      <VenueSection lang={lang} className="relative mx-auto lg:w-5/8 w-10/12 mt-20" />
+      <VenueDetailSection lang={lang} className="relative mx-auto lg:w-5/8 w-10/12 mt-20" />
+    </DefaultLayout>
+  )
+}
+
+export default VenuePage;
