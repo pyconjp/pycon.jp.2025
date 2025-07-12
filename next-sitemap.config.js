1f08964a22a7212b24b078963de287cef4b1c21c
k-nakamura <koya.nakamura@gmail.com>
2025-07-12 16:10:32 +0900

    use next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_PRODUCTION_URL || process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+};
