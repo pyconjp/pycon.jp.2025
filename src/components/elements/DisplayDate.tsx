de616648b7cd978689cccb0d8ce5488e7f40fd46
k-nakamura <koya.nakamura@gmail.com>
2025-09-14 16:17:36 +0900

    add new keynote details and schedule links to KeynotesSection; enhance DisplayDate with optional link support for timetable navigation

--- src/components/elements/DisplayDate.tsx
+++ src/components/elements/DisplayDate.tsx
@@ -3,40 +3,46 @@ import { Lang } from "@/types/lang";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import clsx from "clsx";
+import Link from "next/link";
 
 export default function DisplayDate({ lang }: { lang: Lang }) {
   const dict = dictionary[lang];
-  const DateArea = ({ day, month, date, weekday, className }: {
+  const DateArea = ({ day, month, date, weekday, className, href }: {
     day: string,
     month: string,
     date: string,
     weekday: string,
-    className: string
-  }) => (
-    <div className={clsx('flex-1 rounded-lg py-4 lg:py-6 text-center', className)}>
-      <div className='w-full text-xl'>
-        {day}
-      </div>
-      <div className='relative lg:mt-8 mt-2 w-20 lg:h-24 h-16 mx-auto'>
-        <div className='absolute top-0 left-0 text-xl'>
-          {month}
+    className: string,
+    href?: string
+  }) => {
+    const content = (
+      <div className={clsx('flex-1 rounded-lg py-4 lg:py-6 text-center', className, href && 'cursor-pointer')}>
+        <div className='w-full text-xl'>
+          {day}
         </div>
-        <hr className='absolute top-6 w-10/12 [transform:rotate(-25deg)]' />
-        <div className='absolute bottom-0 right-0 left-0 lg:text-6xl text-4xl text-right lg:text-center'>
-          {date}
+        <div className='relative lg:mt-8 mt-2 w-20 lg:h-24 h-16 mx-auto'>
+          <div className='absolute top-0 left-0 text-xl'>
+            {month}
+          </div>
+          <hr className='absolute top-6 w-10/12 [transform:rotate(-25deg)]' />
+          <div className='absolute bottom-0 right-0 left-0 lg:text-6xl text-4xl text-right lg:text-center'>
+            {date}
+          </div>
+        </div>
+        <div className='w-full lg:mt-4 mt-2 text-sm'>
+          {weekday}
         </div>
       </div>
-      <div className='w-full lg:mt-4 mt-2 text-sm'>
-        {weekday}
-      </div>
-    </div>
-  )
+    );
+
+    return href ? <Link href={href} className='flex-1'>{content}</Link> : content;
+  }
   return (
     <div className='font-jost'>
       <div className='flex flex-col w-80 font-semibold ml-auto mx-auto lg:mr-0'>
         <div className='flex flex-row lg:h-60 h-40'>
-          <DateArea day='DAY 1' month='09' date='26' weekday='FRI' className='bg-secondary text-black' />
-          <DateArea day='DAY 2' month='09' date='27' weekday='SAT' className='bg-primary text-white' />
+          <DateArea day='DAY 1' month='09' date='26' weekday='FRI' className='bg-secondary text-black' href={`/${lang}/timetable/day1`} />
+          <DateArea day='DAY 2' month='09' date='27' weekday='SAT' className='bg-primary text-white' href={`/${lang}/timetable/day2`} />
         </div>
         <div
           className='bg-gray-50 rounded-lg h-12 lg:h-16 border-gray-300 border-2 flex justify-between items-center px-6'>
